ed2cf8eb466efd0c4e7d178cf689f8282ba8769e
Vladimir Bedash <vvbedash@gmail.com>
2023-02-08 15:09:48 +0600

    Initial commit

---
+++ SSCWPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace SSCWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Image1.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+        
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Image2.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+    }
+}
